5692b1b4c126b5d4eb8ed1493e323d72d8d1a109
Krist Pornpairin <krist7599555@gmail.com>
2020-04-05 19:24:28 +0700

    🍃config tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,24 @@
 module.exports = {
   theme: {
-    extend: {},
+    container: {
+      center: true
+    },
+    fontFamily: {
+      'sans': ['Chula', '-apple-system', 'Helvetica', 'Arial', 'sans-serif'],
+      'serif': ['Chula', 'Georgia', 'Cambria', 'serif'],
+    },
+    screens: {
+      'sm': '640px', // => @media (min-width: 640px) { ... }
+      'md': '768px', // => @media (min-width: 768px) { ... }
+      'lg': '1024px', // => @media (min-width: 1024px) { ... }
+      // 'xl': '1280px', // => @media (min-width: 1280px) { ... }
+    },
+    extend: {
+      colors: {
+        white: "#FFFFFF",
+        primary: "#c74851"
+      }
+    }
   },
   variants: {},
   plugins: [],
